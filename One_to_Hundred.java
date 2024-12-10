fc9d8bab94f6eb7340cf8039b3ae6981d5884dfb
cherishma2124 <cherishmayerramada@gmail.com>
2024-12-11 00:54:13 +0530

    initial 2

--- One_to_Hundred.java
+++ One_to_Hundred.java
@@ -1,6 +1,6 @@
 public class One_to_Hundred {
     public static void main(String[] args) {
-        int num = 100;
+        int num = 15;
         boolean istrue = false;
         for (int i = 2; i <= num / 2; i++) {
             if (i % num == 0) {
