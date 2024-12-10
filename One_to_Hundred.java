7381ab5f8a55824385e990c7139376e6373e654e
cherishma2124 <cherishmayerramada@gmail.com>
2024-12-11 00:52:38 +0530

    initial

---
+++ One_to_Hundred.java
@@ -0,0 +1,19 @@
+public class One_to_Hundred {
+    public static void main(String[] args) {
+        int num = 100;
+        boolean istrue = false;
+        for (int i = 2; i <= num / 2; i++) {
+            if (i % num == 0) {
+                istrue = true;
+                break;
+            }
+
+        }
+        if (istrue) {
+            System.out.println(num + "is a prime");
+
+        } else {
+            System.out.println(num + "is not a prime");
+        }
+    }
+}
